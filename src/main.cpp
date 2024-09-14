3fa3843e751219d25ca82696c6729798d8386369
BensonChen <vince7ben23@gmail.com>
2024-09-14 18:02:14 +0000

    add functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "common/common.hpp"
+#include <queue>
+#include "command/commands.hpp"
 
 using namespace std;
 
@@ -29,54 +30,136 @@ insert(copy(1, find(2, 1), 2), 2, 2)
 --------------------------------------------------
 */
 
-vector<string> strings;
+void parse_commands_and_args(const char *ch, queue<string> &commands_and_args);
 
-int main() {
-	
+bool process_commands(queue<string> &commands_and_args, vector<string> &strings);
+
+string extract_args(queue<string> &commands_and_args, vector<string> &strings);
+
+int main()
+{
+
+	vector<string> strings;
+	queue<string> commands_and_args;
+
 	int num;
 	cin >> num;
 
-	for (int i = 0; i < num; ++i) {
+	for (int i = 0; i < num; ++i)
+	{
 		string input;
 		cin >> input;
 		strings.push_back(input);
 	}
-	
 
-	/*
-	for (vector<string>::const_iterator i = strings.begin(); i != strings.end(); ++i) {
-		cout << *i << endl;
-	}*/
+	cin.clear();   // reset to goodbit
+	cin.ignore(1); // extract and discard '\n' characters
 
-	/*
-	string str = copy(1, 2, 5, strings);
-	cout << str << endl;
-	*/
+	char command[101];
+	while (true)
+	{
+		cin.getline(command, 100, '\n');
+		// cout << "command: " << command << endl;
+		parse_commands_and_args(command, commands_and_args);
+		bool res = process_commands(commands_and_args, strings);
+		if (!res)
+			break;
+	}
 
-	/*
-	int res = find(string("abc"), 1, strings);
-	cout << res << endl;
-	*/
+	// cout << commands_and_args.size() << endl;
+	// while(!commands_and_args.empty()) {
+	// 		cout << commands_and_args.front() << endl;
+	// 		commands_and_args.pop();
+	// }
 
-	int res2 = rfind(string("def"), 1, strings);
-	cout << res2 << endl;
-
-
-	/*
-	string str1 = "abc";
-	string str2 = "def";
-	cout << add(str1, str2) << endl;
-
-	string str3 = "123";
-	string str4 = "456";
-	cout << add(str3, str4) << endl;
-
-	string str5 = "-100000";
-	string str6 = "-100000";
-	cout << add(str5, str6) << endl;
-	*/
-
-	cout << "main func end!!" << endl;
+	// cout << "main func end!!" << endl;
 
 	return 0;
+}
+
+void parse_commands_and_args(const char *ch, queue<string> &commands_and_args)
+{
+
+	string tmp;
+
+	for (int i = 0; ch[i] != '\0'; ++i)
+	{
+		if (ch[i] != ' ')
+		{
+			tmp.push_back(ch[i]);
+		}
+		else
+		{
+			commands_and_args.push(tmp);
+			tmp.clear();
+		}
+	}
+	commands_and_args.push(tmp);
+}
+
+bool process_commands(queue<string> &commands_and_args, vector<string> &strings)
+{
+	string str = commands_and_args.front();
+	commands_and_args.pop();
+
+	if (str == "insert")
+	{
+		string inserted_str = extract_args(commands_and_args, strings);
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		int pos = stoi(extract_args(commands_and_args, strings));
+
+		insert(inserted_str, str_no, pos, strings);
+	}
+	else if (str == "reset")
+	{
+		string new_str = extract_args(commands_and_args, strings);
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		reset(new_str, str_no, strings);
+	}
+	else if (str == "print")
+	{
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		print(str_no, strings);
+	}
+	else if (str == "printall")
+	{
+		printall(strings);
+	}
+	else
+		return false;
+	return true;
+}
+
+string extract_args(queue<string> &commands_and_args, vector<string> &strings)
+{
+	string str = commands_and_args.front();
+	commands_and_args.pop();
+
+	if (str == "copy")
+	{
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		int pos = stoi(extract_args(commands_and_args, strings));
+		int len = stoi(extract_args(commands_and_args, strings));
+		return copy(str_no, pos, len, strings);
+	}
+	else if (str == "add")
+	{
+		string str1 = extract_args(commands_and_args, strings);
+		string str2 = extract_args(commands_and_args, strings);
+		return add(str1, str2);
+	}
+	else if (str == "find")
+	{
+		string target = extract_args(commands_and_args, strings);
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		return to_string(find(target, str_no, strings));
+	}
+	else if (str == "rfind")
+	{
+		string target = extract_args(commands_and_args, strings);
+		int str_no = stoi(extract_args(commands_and_args, strings));
+		return to_string(rfind(target, str_no, strings));
+	}
+	else
+		return str;
 }
